3bdc559f900358ed1bf56ed3c03d7310727ac73b
aczietlow <aczietlow@gmail.com>
2025-09-04 17:11:01 -0400

    adding zen browser to build

--- install/software/desktop.sh
+++ install/software/desktop.sh
@@ -4,11 +4,11 @@
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
 ## Zen Browser
-# sudo dnf copr -y enable architektapx/zen-browser fedora-42-aarch64
-# sudo dnf install zen-browser
+sudo dnf copr enable -y sneexy/zen-browser
 
 sudo dnf check-update
 
 sudo dnf install -y \
-  brave-browser 
+  brave-browser zen-browser 
+
 
