a94421f17d3f7469a3e631e0daff45de16624e4c
aczietlow <aczietlow@gmail.com>
2025-11-01 00:01:00 -0400

    type

--- install/desktop/steam.sh
+++ install/desktop/steam.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-sudo dnf instal2l steam -y
+sudo dnf install steam -y
 
 # do proton via the cli
 pip3 install --user protonup
 mkdir -p ~/.steam/root/compatibilitytools.d
 protonup -y -d ~/.steam/root/compatibilitytools.d/
 
-protonup -d ~/.steam/root/compatibilitytools.d
+protonup 
 
 # sudo dnf copr enable apicalshark/ProtonUp-Qt -y
 # sudo dnf install protonup-qt -y
